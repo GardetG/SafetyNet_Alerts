234c0fd0c9fe6347cdda6e2cc4d475750bb414fa
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-23 13:42:16 +0100

    Add GlobalControllerExceptionHandler handling for invalid DTP

--- src/main/java/com/safetynet/alerts/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/safetynet/alerts/controller/GlobalControllerExceptionHandler.java
@@ -2,12 +2,16 @@ package com.safetynet.alerts.controller;
 
 import com.safetynet.alerts.exception.ResourceNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,7 +33,8 @@ public class GlobalControllerExceptionHandler {
    * @return HTTP 400 response with informations on invalid parameters
    */
   @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<List<String>> handleValidation2Exceptions(ConstraintViolationException ex) {
+  public ResponseEntity<List<String>> handleParameterValidationExceptions(
+          ConstraintViolationException ex) {
     List<String> errors = new ArrayList<>();
     ex.getConstraintViolations().forEach(error -> {
       String errorMessage = error.getMessage();
@@ -37,6 +42,27 @@ public class GlobalControllerExceptionHandler {
     });
     LOGGER.error("Can't process input : invalid parameter {}", errors);
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+  }
+  
+  /**
+   * Handle MethodArgumentNotValidException thrown when trying to deserialize an
+   * invalid object.
+   * 
+
+   * @param ex instance of the exception
+   * @return HTTP 422 response with informations on invalid fields
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+          MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    LOGGER.error("Can't process input data : invalid ressource");
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errors);
   }
 
   /**
@@ -48,7 +74,7 @@ public class GlobalControllerExceptionHandler {
    * @return HTTP 400 response with informations on invalid parameters
    */
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<String> handleValidation2Exceptions(ResourceNotFoundException ex) {
+  public ResponseEntity<String> handleNotFoundExceptions(ResourceNotFoundException ex) {
     String error =  ex.getMessage();
     LOGGER.error("Can't find the resource : {}", error);
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
