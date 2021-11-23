c7f7a0a3fec2a28dce626aed9e4e5c04e231170f
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-23 13:48:50 +0100

    Add PersonController POST method

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,9 +1,12 @@
 package com.safetynet.alerts.controller;
 
 import com.safetynet.alerts.dto.PersonDto;
+import com.safetynet.alerts.exception.ResourceAlreadyExistsException;
 import com.safetynet.alerts.exception.ResourceNotFoundException;
 import com.safetynet.alerts.service.PersonService;
+import java.net.URI;
 import java.util.List;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -49,18 +54,41 @@ public class PersonController {
    * @param firstName of the person
    * @param lastName  of the person
    * @return HTTP 200 Response with the person
+   * @throws ResourceNotFoundException when the person is not found
    */
+  
   @GetMapping("/persons/person")
   public ResponseEntity<PersonDto> getPerson(
           @RequestParam @NotBlank(message = "Firstname is mandatory") String firstName,
           @RequestParam @NotBlank(message = "LastName is mandatory") String lastName)
           throws ResourceNotFoundException {
     
-    LOGGER.info("Request: Get persons with parameter: {}, {}", firstName, lastName);
+    LOGGER.info("Request: Get persons with parameters: {}, {}", firstName, lastName);
     PersonDto person = personService.getByName(firstName, lastName);
     LOGGER.info("Response: person sent");
     return ResponseEntity.ok(person);
     
   }
 
+  /**
+   * Handle HTTP POST request for a person resource.
+   * 
+
+   * @param person to create
+   * @return HTTP 201 Response with the person created
+   * @throws ResourceAlreadyExistsException when the person already exists
+   */
+  @PostMapping("/person")
+  public ResponseEntity<PersonDto> postPerson(@Valid @RequestBody PersonDto person) 
+          throws ResourceAlreadyExistsException {
+
+    LOGGER.info("Request: Create person {} {}", person.getFirstName(), person.getLastName());
+    PersonDto createdPerson = personService.add(person);
+
+    URI uri = URI.create("/persons/person?firstName=" + createdPerson.getFirstName() + "&lastName="
+            + createdPerson.getLastName());
+    LOGGER.info("Response: person created");
+    return ResponseEntity.created(uri).body(createdPerson);
+  }
+  
 }
