a7da19ae05bb3a78b06c41ad3adb7fbbcbd26fc9
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-23 13:43:12 +0100

    Add PersonDto equals() override

--- src/main/java/com/safetynet/alerts/dto/PersonDto.java
+++ src/main/java/com/safetynet/alerts/dto/PersonDto.java
@@ -1,5 +1,6 @@
 package com.safetynet.alerts.dto;
 
+import java.util.Objects;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,4 +26,26 @@ public class PersonDto {
   private String phone;
   private String email;
   
+  @Override
+  public int hashCode() {
+    return Objects.hash(address, city, email, firstName, lastName, phone, zip);
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    PersonDto other = (PersonDto) obj;
+    return Objects.equals(address, other.address) && Objects.equals(city, other.city)
+            && Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+            && Objects.equals(lastName, other.lastName) && Objects.equals(phone, other.phone)
+            && Objects.equals(zip, other.zip);
+  }
 }
