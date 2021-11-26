2f1605bcde7c5dafb86c6944afa19c7b6df75535
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-26 17:00:55 +0100

    Add MedicalRecordMapper

---
+++ src/main/java/com/safetynet/alerts/dto/MedicalRecordMapper.java
@@ -0,0 +1,69 @@
+package com.safetynet.alerts.dto;
+
+import com.safetynet.alerts.model.MedicalRecord;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Mapping Class between MedicalRecord and MedicalRecordDto.
+ */
+public class MedicalRecordMapper {
+
+  private MedicalRecordMapper() {
+    throw new IllegalStateException("Utility class");
+  }
+  
+  /**
+   * Map MedicalRecord into MedicalRecordDto.
+   * 
+
+   * @param medicalRecord to map
+   * @return DTO associated
+   */
+  public static MedicalRecordDto toDto(MedicalRecord medicalRecord) {
+    if (medicalRecord == null) {
+      return null;
+    }
+    MedicalRecordDto medicalRecordDto = new MedicalRecordDto();
+    medicalRecordDto.setFirstName(medicalRecord.getFirstName());
+    medicalRecordDto.setLastName(medicalRecord.getLastName());
+    medicalRecordDto.setBirthdate(medicalRecord.getBirthdate());
+    medicalRecordDto.setMedications(medicalRecord.getMedications());
+    medicalRecordDto.setAllergies(medicalRecord.getAllergies());
+    return medicalRecordDto;
+  }
+  
+  /**
+   * Map list of MedicalRecord into list of MedicalRecordDto.
+   * 
+
+   * @param medicalRecords to map
+   * @return DTO associated
+   */
+  public static List<MedicalRecordDto> toDto(List<MedicalRecord> medicalRecords) {
+    return medicalRecords.stream()
+            .map(MedicalRecordMapper::toDto)
+            .collect(Collectors.toList());
+  }
+  
+  /**
+   * Map MedicalRecordDto into MedicalRecord.
+   * 
+
+   * @param medicalRecordDto to map
+   * @return MedicalRecord associated
+   */
+  public static MedicalRecord toModel(MedicalRecordDto medicalRecordDto) {
+    if (medicalRecordDto == null) {
+      return null;
+    }
+    MedicalRecord medicalRecord = new MedicalRecord();
+    medicalRecord.setFirstName(medicalRecordDto.getFirstName());
+    medicalRecord.setLastName(medicalRecordDto.getLastName());
+    medicalRecord.setBirthdate(medicalRecordDto.getBirthdate());
+    medicalRecord.setMedications(medicalRecordDto.getMedications());
+    medicalRecord.setAllergies(medicalRecordDto.getAllergies());
+    return medicalRecord;
+  }
+  
+}
