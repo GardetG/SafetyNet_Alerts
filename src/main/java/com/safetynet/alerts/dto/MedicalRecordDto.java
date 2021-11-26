cc1d2e4c484fa2e474f8ea92964b11bff257da00
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-26 17:47:07 +0100

    Update MedicalRecordDto to parse json date with correct format

--- src/main/java/com/safetynet/alerts/dto/MedicalRecordDto.java
+++ src/main/java/com/safetynet/alerts/dto/MedicalRecordDto.java
@@ -1,5 +1,7 @@
 package com.safetynet.alerts.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
@@ -21,6 +23,7 @@ public class MedicalRecordDto {
   private String firstName;
   @NotBlank(message = "Lastname is mandatory")
   private String lastName;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
   private LocalDate birthdate;
   private List<String> medications;
   private List<String> allergies;
