8ca74fc243a4bf6110681da67f55e1ca06417100
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-11-28 21:42:00 +0100

    Implement AlertController GET /fire method

--- src/main/java/com/safetynet/alerts/controller/AlertsController.java
+++ src/main/java/com/safetynet/alerts/controller/AlertsController.java
@@ -1,6 +1,7 @@
 package com.safetynet.alerts.controller;
 
 import com.safetynet.alerts.dto.ChildAlertDto;
+import com.safetynet.alerts.dto.FireAlertDto;
 import com.safetynet.alerts.dto.PersonInfoDto;
 import com.safetynet.alerts.exception.ResourceNotFoundException;
 import com.safetynet.alerts.service.AlertsService;
@@ -81,7 +82,7 @@ public class AlertsController {
 
    * @param firstName of the person
    * @param lastName of the person
-   * @return HTTP 200 Response with a list of phone info
+   * @return HTTP 200 Response with a list persons informations
    * @throws ResourceNotFoundException when the person is not found
    */
   @GetMapping("/personInfo")
@@ -100,12 +101,12 @@ public class AlertsController {
   
   /**
    * Handle HTTP GET request on /childAlert and return child alert information with
-   * a lits of childs of the household with their age and the list of others household memebers.
+   * a list of children of the household with their age and the list of others household members.
    * 
 
    * @param address of the household
-   * @return HTTP 200 Response with a list of phone info
-   * @throws ResourceNotFoundException when the person is not found
+   * @return HTTP 200 Response with child alert informations
+   * @throws ResourceNotFoundException when no resident found at this address
    */
   @GetMapping("/childAlert")
   public ResponseEntity<ChildAlertDto> getChildAlert(
@@ -117,6 +118,28 @@ public class AlertsController {
     
     LOGGER.info("Response: Child alert informations sent");
     return ResponseEntity.ok(childAlertDto);
+
+  }
+  
+  /**
+   * Handle HTTP GET request on /fire and return fire alert information with
+   * a list of the residents with their age and medical data and the associated station.
+   * 
+
+   * @param address of the residents
+   * @return HTTP 200 Response with fire alert informations
+   * @throws ResourceNotFoundException when no resident found at this address
+   */
+  @GetMapping("/fire")
+  public ResponseEntity<FireAlertDto> getFireAlert(
+          @RequestParam @NotBlank(message = "Address is mandatory") String address)
+          throws ResourceNotFoundException {
+    
+    LOGGER.info("Request: Get fire alert information at {}", address);
+    FireAlertDto fireAlertDto = alertsService.fireAlert(address);
+    
+    LOGGER.info("Response: fire alert informations sent");
+    return ResponseEntity.ok(fireAlertDto);
 
   }
   
