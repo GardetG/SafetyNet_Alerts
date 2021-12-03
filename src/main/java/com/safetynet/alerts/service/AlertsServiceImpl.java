adc8cece419ee7ea7497290e81276b4c20778c87
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-12-03 18:29:03 +0100

    Refactor AlertsService to fetch data from repository

--- src/main/java/com/safetynet/alerts/service/AlertsServiceImpl.java
+++ src/main/java/com/safetynet/alerts/service/AlertsServiceImpl.java
@@ -9,13 +9,15 @@ import com.safetynet.alerts.exception.ResourceNotFoundException;
 import com.safetynet.alerts.model.FireStation;
 import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.repository.FireStationRepository;
+import com.safetynet.alerts.repository.MedicalRecordRepository;
+import com.safetynet.alerts.repository.PersonRepository;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,13 +33,13 @@ public class AlertsServiceImpl implements AlertsService {
   private static final Logger LOGGER = LoggerFactory.getLogger(AlertsServiceImpl.class);
 
   @Autowired
-  PersonService personService;
+  PersonRepository personRepository;
 
   @Autowired
-  FireStationService fireStationService;
+  FireStationRepository fireStationRepository;
 
   @Autowired
-  MedicalRecordService medicalRecordService;
+  MedicalRecordRepository medicalRecordRepository;
 
   /**
    * {@inheritDoc}
@@ -45,19 +47,11 @@ public class AlertsServiceImpl implements AlertsService {
   @Override
   public List<String> getCommunityEmail(String city) throws ResourceNotFoundException {
 
-    List<String> emailList = new ArrayList<>();
-
-    try {
-      // Fetch all resident of the city, throw an exception if no resident found
-      emailList = personService.getByCity(city).stream()
-              // Retrieved their email
-              .map(Person::getEmail)
-              // Filter out any duplicate or empty field
-              .distinct().filter(email -> (email != null && !email.isBlank()))
-              .collect(Collectors.toList());
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
-    }
+    // Fetch all resident of the city an retrieve email
+    List<String> emailList = personRepository.findByCity(city).stream()
+            .map(Person::getEmail)
+            .distinct().filter(email -> (email != null && !email.isBlank()))
+            .collect(Collectors.toList());
 
     if (emailList.isEmpty()) {
       String error = String.format("No resident emails found for %s", city);
@@ -73,31 +67,14 @@ public class AlertsServiceImpl implements AlertsService {
   @Override
   public List<String> getPhoneAlert(int station) throws ResourceNotFoundException {
 
-    List<String> phoneList = new ArrayList<>();
-
-    try {
-      // Fetch all mapping of this station, throw an exception if no mapping found
-      phoneList = fireStationService.getByStation(station).stream()
-              // Retrieve addresses covered by the station
-              .map(FireStation::getAddress)
-              // Retrieve all resident for each address
-              .flatMap(address -> {
-                List<Person> persons = new ArrayList<>();
-                try {
-                  persons = personService.getByAddress(address);
-                } catch (ResourceNotFoundException ex) {
-                  LOGGER.warn(ex.getMessage());
-                }
-                return persons.stream();
-              })
-              // retrieve their phone number
-              .map(Person::getPhone)
-              // Filter out any duplicate or empty field
-              .distinct().filter(phone -> (phone != null && !phone.isBlank()))
-              .collect(Collectors.toList());
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
-    }
+    // Fetch all addresses covered by this station
+    List<String> phoneList = fireStationRepository.findByStation(station)
+            .stream()
+            .map(FireStation::getAddress)
+            .flatMap(address -> personRepository.findByAddress(address).stream())
+            .map(Person::getPhone).distinct()
+            .filter(phone -> (phone != null && !phone.isBlank()))
+            .collect(Collectors.toList());
 
     if (phoneList.isEmpty()) {
       String error = String.format("No resident phone number found for station %s", station);
@@ -114,20 +91,21 @@ public class AlertsServiceImpl implements AlertsService {
   public List<PersonInfoDto> getPersonInfo(String firstName, String lastName)
           throws ResourceNotFoundException {
 
-    PersonInfoDto personInfo = new PersonInfoDto();
-
     // Retrieve person information, throw an exception if not found
-    Person person = personService.getByName(firstName, lastName);
-    personInfo.setFirstName(person.getFirstName());
-    personInfo.setLastName(person.getLastName());
-    personInfo.setAddress(person.getAddress());
+    Optional<Person> personInfo = personRepository.findByName(firstName, lastName);
+    if (personInfo.isEmpty()) {
+      String error = String.format("%s %s not found", firstName, lastName);
+      throw new ResourceNotFoundException(error);
+    }
 
-    // retrieve medical record data if it exists
-    Optional<MedicalRecord> medicalRecord = getMedicalRecord(person);
-    mapAge(personInfo, medicalRecord);
-    mapMedicalData(personInfo, medicalRecord);
+    return List.of(personInfo.get()).stream()
+            .map(person -> {
+              Optional<MedicalRecord> medicalRecord = getAsssociateMedicalRecord(person);
+              return createPersonInfoDto(person, medicalRecord,
+                      List.of("address", "email", "age", "medical"));
+            })
+            .collect(Collectors.toList());
 
-    return List.of(personInfo);
   }
 
   /**
@@ -137,39 +115,31 @@ public class AlertsServiceImpl implements AlertsService {
   public ChildAlertDto childAlert(String address) throws ResourceNotFoundException {
 
     // Fetch resident at the address and map them with optional medical record
-    Map<Person, Optional<MedicalRecord>> residents = getPersonWithMedicalRecordFromAddress(address);
+    List<Person> residents = personRepository.findByAddress(address);
 
     if (residents.isEmpty()) {
       String error = String.format("No residents found living at %s", address);
       throw new ResourceNotFoundException(error);
     }
-    
-    List<PersonInfoDto> childrenList = residents.entrySet().stream()
-            // Filter resident with medical record and who are minor
-            .filter(entry -> entry.getValue().isPresent() && entry.getValue().get().isMinor())
-            .map(entry -> {
-              PersonInfoDto childDto = new PersonInfoDto();
-              childDto.setFirstName(entry.getKey().getFirstName());
-              childDto.setLastName(entry.getKey().getLastName());
-              childDto.setAge(String.valueOf(entry.getValue().get().getAge()));
-              return childDto;
-            }).collect(Collectors.toList());
 
-    List<PersonInfoDto> householdMembers = residents.entrySet().stream()
-            // Filter out minor
-            .filter(entry -> !(entry.getValue().isPresent() && entry.getValue().get().isMinor()))
-            .map(entry -> {
-              PersonInfoDto childDto = new PersonInfoDto();
-              childDto.setFirstName(entry.getKey().getFirstName());
-              childDto.setLastName(entry.getKey().getLastName());
-              if (entry.getValue().isEmpty()) {
-                childDto.setAge("Information not specified");
-              }
-              return childDto;
-            }).collect(Collectors.toList());
+    List<PersonInfoDto> children = new ArrayList<>();
+    List<PersonInfoDto> householdMembers = new ArrayList<>();
+
+    residents.forEach(person -> {
+      Optional<MedicalRecord> medicalRecord = getAsssociateMedicalRecord(person);
+      if (medicalRecord.isEmpty()) {
+        householdMembers.add(createPersonInfoDto(person, medicalRecord, List.of("age")));
+        return;
+      }
+      if (medicalRecord.get().isMinor()) {
+        children.add(createPersonInfoDto(person, medicalRecord, List.of("age")));
+      } else {
+        householdMembers.add(createPersonInfoDto(person, medicalRecord, Collections.emptyList()));
+      }
+    });
 
     ChildAlertDto childAlertDto = new ChildAlertDto();
-    childAlertDto.setChildren(childrenList);
+    childAlertDto.setChildren(children);
     childAlertDto.setHouseholdMembers(householdMembers);
 
     return childAlertDto;
@@ -181,19 +151,23 @@ public class AlertsServiceImpl implements AlertsService {
   @Override
   public FireAlertDto fireAlert(String address) throws ResourceNotFoundException {
 
-    // Fetch resident at the address and medical record data if it exists
-    List<PersonInfoDto> residentsList = getPersonInfoFromAddress(address);
+    // Fetch resident at the address and create DTO
+    List<PersonInfoDto> residentsList = personRepository.findByAddress(address).stream()
+            .map(person -> {
+              Optional<MedicalRecord> medicalRecord = getAsssociateMedicalRecord(person);
+              return createPersonInfoDto(person, medicalRecord, List.of("phone", "age", "medical"));
+            })
+            .collect(Collectors.toList());
 
     if (residentsList.isEmpty()) {
       String error = String.format("No residents found living at %s", address);
       throw new ResourceNotFoundException(error);
     }
-    
+
     String station = "No station mapped for this address";
-    try {
-      station = String.valueOf(fireStationService.getByAddress(address).getStation());
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
+    Optional<FireStation> fireStation = fireStationRepository.findByAddress(address);
+    if (fireStation.isPresent()) {
+      station = String.valueOf(fireStation.get().getStation());
     }
 
     FireAlertDto fireAlertDto = new FireAlertDto();
@@ -209,25 +183,31 @@ public class AlertsServiceImpl implements AlertsService {
   @Override
   public List<FloodHouseholdDto> floodAlert(List<Integer> stations)
           throws ResourceNotFoundException {
-    
+
     List<FloodHouseholdDto> floodlist = stations.stream()
-            // Fetch all addresses cover by the stations
-            .flatMap(station -> getAddressListMappedToStation(station.intValue()).stream())
+            .flatMap(station -> fireStationRepository.findByStation(station).stream())
+            .map(FireStation::getAddress)
             .map(address -> {
               // Fetch resident at the address and medical record data if it exists
+              List<PersonInfoDto> residentsList = personRepository.findByAddress(address).stream()
+                      .map(person -> {
+                        Optional<MedicalRecord> medicalRecord = getAsssociateMedicalRecord(person);
+                        return createPersonInfoDto(person, medicalRecord,
+                                List.of("phone", "age", "medical"));
+                      })
+                      .collect(Collectors.toList());
               FloodHouseholdDto floodDto = new FloodHouseholdDto();
               floodDto.setAddress(address);
-              floodDto.setResidents(getPersonInfoFromAddress(address));
+              floodDto.setResidents(residentsList);
               return floodDto;
-            })
-            .filter(household -> !household.getResidents().isEmpty())
+            }).filter(household -> !household.getResidents().isEmpty())
             .collect(Collectors.toList());
 
     if (floodlist.isEmpty()) {
       String error = String.format("No residents covered found for stations %s", stations);
       throw new ResourceNotFoundException(error);
     }
-    
+
     return floodlist;
   }
 
@@ -236,125 +216,95 @@ public class AlertsServiceImpl implements AlertsService {
    */
   @Override
   public FireStationCoverageDto fireStationCoverage(int station) throws ResourceNotFoundException {
-    
+
     // Fetch all resident with optional medical record covered by station
-    Map<Person, Optional<MedicalRecord>> residents = getAddressListMappedToStation(station)
-            .stream()
-            .flatMap(address -> getPersonWithMedicalRecordFromAddress(address).entrySet().stream())
-            .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-    
-    List<PersonInfoDto> residentsList = residents.entrySet().stream()
-            .map(entry -> {
-              PersonInfoDto personDto = new PersonInfoDto();
-              personDto.setFirstName(entry.getKey().getFirstName());
-              personDto.setLastName(entry.getKey().getLastName());
-              personDto.setAddress(entry.getKey().getAddress());
-              personDto.setPhone(entry.getKey().getPhone());
-              return personDto;
-            }).collect(Collectors.toList());
-    
-    if (residentsList.isEmpty()) {
+    List<Person> residents = fireStationRepository.findByStation(station).stream()
+            .map(FireStation::getAddress)
+            .flatMap(address -> personRepository.findByAddress(address).stream())
+            .collect(Collectors.toList());
+
+    if (residents.isEmpty()) {
       String error = String.format("No residents covered found for station %s", station);
       throw new ResourceNotFoundException(error);
     }
-    
-    int childrenCount = residents.entrySet().stream()
-            .filter(entry -> (entry.getValue().isPresent() && entry.getValue().get().isMinor()))
-            .collect(Collectors.toSet()).size();
-    
-    int adultCount = residents.entrySet().stream()
-            .filter(entry -> (entry.getValue().isPresent() && !entry.getValue().get().isMinor()))
-            .collect(Collectors.toSet()).size();
-    
-    int undeterminedAgeCount = residents.entrySet().stream()
-            .filter(entry -> (entry.getValue().isEmpty()))
-            .collect(Collectors.toSet()).size();
-    
+
+    Map<String, Integer> counter = new HashMap<>();
+    counter.put("unknow", 0);
+    counter.put("children", 0);
+    counter.put("adult", 0);
+    List<PersonInfoDto> residentsList = new ArrayList<>();
+
+    residents.forEach(person -> {
+      Optional<MedicalRecord> medicalRecord = getAsssociateMedicalRecord(person);
+      residentsList.add(createPersonInfoDto(person, medicalRecord, List.of("address", "phone")));
+      if (medicalRecord.isEmpty()) {
+        counter.put("unknow", counter.get("unknow") + 1);
+      } else if (medicalRecord.get().isMinor()) {
+        counter.put("children", counter.get("children") + 1);
+      } else {
+        counter.put("adult", counter.get("adult") + 1);
+      }
+    });
+
     FireStationCoverageDto fireStationCoverageDto = new FireStationCoverageDto();
     fireStationCoverageDto.setResidents(residentsList);
-    fireStationCoverageDto.setChildrenCount(childrenCount);
-    fireStationCoverageDto.setAdultCount(adultCount);
+    fireStationCoverageDto.setChildrenCount(counter.get("children"));
+    fireStationCoverageDto.setAdultCount(counter.get("adult"));
     fireStationCoverageDto.setUnderterminedAgeCount(
-            undeterminedAgeCount == 0 ? null : undeterminedAgeCount);
+            counter.get("unknow") == 0 ? null : counter.get("unknow"));
     return fireStationCoverageDto;
   }
-  
-  private List<String> getAddressListMappedToStation(int station) {
-    List<String> addressList = new ArrayList<>();
-    try {
-      addressList = fireStationService
-          .getByStation(station)
-          .stream()
-          .map(FireStation::getAddress)
-          .collect(Collectors.toList());
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
-    }
-    return addressList;
-  }
-  
-  private Map<Person, Optional<MedicalRecord>> getPersonWithMedicalRecordFromAddress(
-          String address) {
-    // Fetch resident at the address and map them with optional medical record
-    Map<Person, Optional<MedicalRecord>> personsWithMedicalRecord = new HashMap<>();
-    try {
-      personsWithMedicalRecord = personService
-            .getByAddress(address)
-            .stream()
-            .collect(Collectors.toMap(Function.identity(), this::getMedicalRecord));
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
-    }
-    return personsWithMedicalRecord;
-  }
-    
-  private Optional<MedicalRecord> getMedicalRecord(Person person) {
-    Optional<MedicalRecord> medicalRecord = Optional.empty();
-    try {
-      medicalRecord = Optional
-              .of(medicalRecordService.getByName(person.getFirstName(), person.getLastName()));
-    } catch (ResourceNotFoundException ex) {
-      LOGGER.warn(ex.getMessage());
+
+
+
+  private Optional<MedicalRecord> getAsssociateMedicalRecord(Person person) {
+    Optional<MedicalRecord> medicalRecord = medicalRecordRepository
+            .findByName(person.getFirstName(), person.getLastName());
+    if (medicalRecord.isEmpty()) {
+      LOGGER.warn("Can't associate any medical record to {} {}", person.getFirstName(),
+              person.getLastName());
     }
     return medicalRecord;
   }
-  
-  private List<PersonInfoDto> getPersonInfoFromAddress(String address) {
-    // Fetch resident at the address and map them with optional medical record
-    Map<Person, Optional<MedicalRecord>> residents = getPersonWithMedicalRecordFromAddress(address);
 
-    return residents.entrySet().stream().map(entry -> {
-      PersonInfoDto personInfo = new PersonInfoDto();
-      personInfo.setFirstName(entry.getKey().getFirstName());
-      personInfo.setLastName(entry.getKey().getLastName());
-      personInfo.setPhone(entry.getKey().getPhone());
-      mapAge(personInfo, entry.getValue());
-      mapMedicalData(personInfo, entry.getValue());
-      return personInfo;
-    }).collect(Collectors.toList());
-  }
+  private PersonInfoDto createPersonInfoDto(Person person, Optional<MedicalRecord> medicalRecord,
+          List<String> args) {
 
-  private void mapAge(PersonInfoDto personInfo,
-          Optional<MedicalRecord> medicalRecord) {
-
-    if (medicalRecord.isEmpty()) {
-      personInfo.setAge("Information not specified");
-    } else {
-      personInfo.setAge(String.valueOf(medicalRecord.get().getAge()));
-    }
-    
-  }
-
-  private void mapMedicalData(PersonInfoDto personInfo,
-          Optional<MedicalRecord> medicalRecord) {
-
-    if (medicalRecord.isEmpty()) {
-      personInfo.setMedications(List.of("Information not specified"));
-      personInfo.setAllergies(List.of("Information not specified"));
-    } else {
-      personInfo.setMedications(medicalRecord.get().getMedications());
-      personInfo.setAllergies(medicalRecord.get().getAllergies());
-    }
+    PersonInfoDto personDto = new PersonInfoDto();
+    personDto.setFirstName(person.getFirstName());
+    personDto.setLastName(person.getLastName());
+    args.forEach(arg -> {
+      switch (arg) {
+        case "address":
+          personDto.setAddress(person.getAddress());
+          break;
+        case "phone":
+          personDto.setPhone(person.getPhone());
+          break;
+        case "email":
+          personDto.setEmail(person.getEmail());
+          break;
+        case "age":
+          if (medicalRecord.isPresent()) {
+            personDto.setAge(String.valueOf(medicalRecord.get().getAge()));
+            break;
+          }
+          personDto.setAge("Information not specified");
+          break;
+        case "medical":
+          if (medicalRecord.isPresent()) {
+            personDto.setMedications(medicalRecord.get().getMedications());
+            personDto.setAllergies(medicalRecord.get().getAllergies());
+            break;
+          }
+          personDto.setMedications(List.of("Information not specified"));
+          personDto.setAllergies(List.of("Information not specified"));
+          break;
+        default:
+          break;
+      }
+    });
+    return personDto;
   }
 
 }
